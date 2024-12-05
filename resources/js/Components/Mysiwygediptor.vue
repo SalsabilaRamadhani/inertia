5ebe4355ae93d054974abbe9c3eb5847a92af329
salma zulala <zulalasalma@gmail.com>
2024-12-05 17:22:46 +0700

    first commit

---
+++ resources/js/Components/Mysiwygediptor.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+      <div ref="editor"></div>
+    </div>
+  </template>
+  
+  <script>
+  import Quill from 'quill';
+  import 'quill/dist/quill.snow.css';
+  
+  export default {
+    name: 'WysiwygEditor',
+    props: ['content'],
+    emits: ['update:content'],
+    mounted() {
+      this.editor = new Quill(this.$refs.editor, {
+        theme: 'snow',
+      });
+  
+      this.editor.on('text-change', () => {
+        this.$emit('update:content', this.editor.root.innerHTML);
+      });
+  
+      if (this.content) {
+        this.editor.root.innerHTML = this.content;
+      }
+    },
+  };
+  </script>
+  
